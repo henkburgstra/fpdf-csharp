6455d1a44d7f6101fbaae6cff0d5f71df6105f51
Henk Burgstra <henk.burgstra@gmail.com>
2020-01-26 21:26:54 +0100

    Class Util with method Utf8ToUtf16

---
+++ fpdf-csharp/Utils/Util.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FpdfCsharp.Utils
+{
+    public static class Util
+    {
+        public static string Utf8ToUtf16(string input, bool withBOM = false)
+        {
+            // TODO: support BOM
+            return Encoding.Unicode.GetString(Encoding.Convert(Encoding.UTF8, Encoding.Unicode, Encoding.UTF8.GetBytes(input)));
+        }
+    }
+}
