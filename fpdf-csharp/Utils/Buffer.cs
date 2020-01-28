5a007c23faa5f87ae97df4952df544f6e2b1ec97
Henk Burgstra <henk.burgstra@gmail.com>
2020-01-28 10:20:28 +0100

    Added Buffer, removed FmtBuffer

---
+++ fpdf-csharp/Utils/Buffer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FpdfCsharp.Utils
+{
+    public class Buffer
+    {
+        private MemoryStream stream;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private long readPos;
+        private long writePos;
+
+        private void Init()
+        {
+            this.readPos = stream.Position;
+            this.writePos = stream.Position;
+            this.reader = new StreamReader(stream);
+            this.writer = new StreamWriter(stream);
+        }
+
+        public Buffer()
+        {
+            stream = new MemoryStream();
+        }
+
+        public void Write(byte[] data)
+        {
+            stream.Seek(0, SeekOrigin.End);
+            stream.Write(data, 0, data.Length);
+        }
+
+        public void WriteString(string data)
+        {
+            stream.Seek(0, SeekOrigin.End);
+            writer.Write(data);
+        }
+    }
+}
